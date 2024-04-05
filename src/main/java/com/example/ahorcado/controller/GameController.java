d8d29e1ffe9d5aa3c7e84d137bc67c1966fcc1f1
Steven B. O. <62767175+iMrStevenS2@users.noreply.github.com>
2024-04-04 19:40:00 -0500

    Updt gameController, game-view

--- src/main/java/com/example/ahorcado/controller/GameController.java
+++ src/main/java/com/example/ahorcado/controller/GameController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -24,104 +25,80 @@ public class GameController {
     private Word word;
 
     @FXML
+    private HBox hBoxChoiceLayout;
+    @FXML
     private HBox hBoxLayout;
     @FXML
     private TextField TextFieldCreated;
 
     @FXML
     private TextField afterCreatedFields;
-//    @FXML
-//    private TextField wordTxt;
+    // @FXML
+    // private TextField wordTxt;
 
     @FXML
 
     private ArrayList<TextField> test = new ArrayList<>();
 
-
     public void crearCamposTextoPalabraSecreta() {
         String secretWord = word.getWordSecret();
 
-        for (int i = 0; i < secretWord.length(); i++) {
+        for (int i = 1; i < secretWord.length() + 1; i++) {
             TextField wordTxt = new TextField();
             wordTxt.setId(String.valueOf(i));
-//            TextField afterCreatedFields = new TextField();
+            // TextField afterCreatedFields = new TextField();
             TextFieldCreated = wordTxt;
             wordTxt.setPrefWidth(40);
+            wordTxt.setEditable(false);
             hBoxLayout.getChildren().add(wordTxt);
-//            System.out.println("added camp " + i);
-//            System.out.println(wordTxt.getId());
-            //manejarPulsacionTecla(wordTxt, i);
+            // System.out.println("added camp " + i);
+            // System.out.println(wordTxt.getId());
+            // manejarPulsacionTecla(wordTxt, i);
             test.add(TextFieldCreated);
         }
     }
 
-
     public void verifyWord(Word word) {
+        int intentos = 6;
+        int fallo = 0;
         String receivedWord = word.getWordSecret();
-        String[] strippedWord = receivedWord.split("");
+        ObservableList<Node> sBox = hBoxChoiceLayout.getChildren();
+        TextField txtChoice = (TextField) sBox.get(0);
+        System.out.println(txtChoice.getText());
+
+        String[] splittedWord = receivedWord.split("");
         ArrayList<String> wordList = new ArrayList<String>();
-//        System.out.println("STRIPPED WORD : ");
-        for (int i = 0; i < strippedWord.length; i++) {
-            wordList.add(strippedWord[i]);
-//            System.out.println(wordList.add(strippedWord[i]));
-//            System.out.println("Letra del Array:");
-//            System.out.println(wordList.get(i));
-        }
-//        System.out.println(wordList);
-//        System.out.println(wordList);
-//        "For" curioso qué funciona
-//        for (String a : strippedWord){
-//            System.out.println(a);
-//        }
-//        for (Node node : hBoxLayout.getChildren()) {
-//            System.out.println(node);
-//        }
-        for (int i = 0; i < receivedWord.length(); i++) {
-//            String boxLetter = String.valueOf(TextFieldCreated.getText());
-//            System.out.println(boxLetter);
-//            System.out.printf("Letter: " + boxLetter);
-//            System.out.println(test);
-//            System.out.println(hBoxLayout.getChildren().get(i));
-//            System.out.println("FUNCTION getTexts:");
-            ObservableList<Node> childs = hBoxLayout.getChildren();
-            TextField tf  = (TextField) childs.get(i);
-//            System.out.println("Text From Fields");
-//            System.out.println(tf.getText());
-//            System.out.println(wordList.get(i));
-//            getTexts();
-            String textOnField = tf.getText();
-            String textReceived = wordList.get(i);
-            if (textReceived.equalsIgnoreCase(textOnField)) {
-                /**
-                 *
-                 * Only for Debug Purposes
-                 * @iMrSteven was here ;)
-                 * */
-                System.out.println("Una letra encaja!");
-                /**
-                 * End Debug
-                 * */
 
-                tf.setDisable(true);
-            } else {
-                /**
-                 *
-                 * Only for Deving Purposes
-                 *
-                * */
-//                System.out.println(textOnField.compareTo(textReceived));
-//                System.out.println(textReceived + " Es Diferente de: " + textOnField + " ???");
-                /**
-                 * End Debug
-                 * */
-                System.out.println("Una letra no encajó!");
+        String choiceWord = txtChoice.getText();
 
+        if (choiceWord.length() > 1) {
+            System.out.println("PALABRA LARGA");
+        } else if (!choiceWord.isEmpty() && choiceWord.matches("[a-zA-Z]+")) {
+
+            // System.out.println("STRIPPED WORD : ");
+            for (int i = 0; i < splittedWord.length; i++) {
+                wordList.add(splittedWord[i]);
+            }
+
+            for (int i = 0; i < wordList.size(); i++) {
+                ObservableList<Node> childs = hBoxLayout.getChildren();
+                TextField tf = (TextField) childs.get(i);
+
+                if (choiceWord.equalsIgnoreCase(wordList.get(i))) {
+                    tf.setText(choiceWord);
+                    fallo = 0;
+                } else {
+                    fallo = fallo + 1;
+
+                }
 
             }
 
+            if (fallo != 0) {
+                intentos = intentos - 1;
+                System.out.println("quedan" + intentos + "");
+            }
         }
-
-
 
     }
 
@@ -143,7 +120,5 @@ public class GameController {
         crearCamposTextoPalabraSecreta(); // Crea los TextFields al asignar la palabra
 
     }
-
-
 
 }
